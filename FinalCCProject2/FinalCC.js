120ca4836a51c9685e9dbe3f042154ff4da48400
elelx <elvaschoo@gmail.com>
2025-05-07 05:48:15 -0400

    ai=udo nad css

--- FinalCCProject2/FinalCC.js
+++ FinalCCProject2/FinalCC.js
@@ -1,61 +1,70 @@
+//connect API
 const catFacts = "https://catfact.ninja/fact";
 
-
+const sound = document.getElementById('mySound');
 
 let theAnswer = "";
 
-
+//button
 document.getElementById("answer").addEventListener("submit", function(event) {
+  sound.play();
 
 //https://www.w3schools.com/jsref/event_preventdefault.asp 
   event.preventDefault();
   
   
-  theAnswer = document.getElementById("feeling").value.trim().toLowerCase(); //value trim --> input + delete thr spaces
+  theAnswer = document.getElementById("confession").value.trim().toLowerCase(); //value trim --> input + delete thr spaces
+
+  //NEED ANSWER OR IAMGE WONT SHOW
+  if (!theAnswer) {
+    alert("Please type something first!");
+    return;
+  }
 
   document.getElementById("result").innerHTML = ""; //1 iamge + clears page
 
+
+//API
   fetch(catFacts)
     .then(response => response.json())
     .then(handlecatFact)
     .catch(error => console.log(error));
 });
 
+//API
 function getCatImage(word) {
+  // you "confession" on the image
   return `https://cataas.com/cat/says/${word}?size=50&color=black`;
 
-  document.getElementById("result").innerHTML = "";
 }
 
+//cat facts + imsge API 
 function handlecatFact(data) {
+  //FACTS_______________________________
   const fact = data.fact;
-
-
-
   const results = document.getElementById("result");
 
-        //showing the catfact 
   const message = document.createElement("p");
-  message.textContent = `heres a interesting cat fact for you,  ${fact}`;
+  message.textContent = `HERE IS A CAT FACT:  ${fact}`;
 
+  //needed help this is so i can css it
+  message.id = "catFact"; 
 
   results.appendChild(message);
 
+// CAT IMGES --------------------______________-
 
-  fetch(getCatImage(theAnswer))
-    .then(response => response.blob())
-    .then(showCatImage)
-    .catch(error => console.log(error));
+  const existingImg = document.getElementById("catImage");
 
-}
-
-function showCatImage(imageBlob) {
-
-
-  const results = document.getElementById("result");
   const img = document.createElement("img");
 
   
-  img.src = URL.createObjectURL(imageBlob);
+  img.id = "catImage";
+
+  //confesssion
+  img.src = getCatImage(theAnswer);
+
   results.appendChild(img);
 }
+
+
