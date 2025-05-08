ae96e5cc8ab1ca91361dbb4f6be79bb10efc13d6
elelx <elvaschoo@gmail.com>
2025-05-08 03:15:37 -0400

    commebts

--- FinalCCProject2/FinalCC.js
+++ FinalCCProject2/FinalCC.js
@@ -14,12 +14,12 @@ document.getElementById("answer").addEventListener("submit", function(event) {
   sound.play();
 
 //https://www.w3schools.com/jsref/event_preventdefault.asp 
-  event.preventDefault();
+  event.preventDefault(); // the image will load 
   
-  
+  //the confession input 
   theAnswer = document.getElementById("confession").value.trim().toLowerCase(); //value trim --> input + delete thr spaces
 
-  //NEED ANSWER OR IAMGE WONT SHOW
+  //cheCcKs NEED ANSWER OR IAMGE WONT SHOW
   if (!theAnswer) {
     alert("Please type something first!");
     return;
@@ -28,7 +28,7 @@ document.getElementById("answer").addEventListener("submit", function(event) {
   document.getElementById("result").innerHTML = ""; //1 iamge + clears page
 
 
-//API
+//API for a ranodm cat fact
   fetch(catFacts)
     .then(response => response.json())
     .then(handlecatFact)
@@ -44,30 +44,28 @@ function getCatImage(word) {
 
 //cat facts + imsge API 
 function handlecatFact(data) {
-  //FACTS_______________________________
+
+  //random caT FACTS when PAGE RELODS _______________________________
   const fact = data.fact;
   const results = document.getElementById("result");
 
   const message = document.createElement("p");
   message.textContent = `HERE IS A CAT FACT:  ${fact}`;
-
   //needed help this is so i can css it
   message.id = "catFact"; 
 
   results.appendChild(message);
+
 
 // CAT IMGES --------------------______________-
 
   const existingImg = document.getElementById("catImage");
 
   const img = document.createElement("img");
-
-  
   img.id = "catImage";
 
   //confesssion
-  img.src = getCatImage(theAnswer);
-
+  img.src = getCatImage(theAnswer); //GET CAT IMAGE (WORD ) IMPLENTS IT ONTO THE IMGE
 
   results.appendChild(img);
 }
